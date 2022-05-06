fc6757d10f35272e917cce090dcb41f4b62c23a2
Matthew S Knox <91904135+MatthewKnox22@users.noreply.github.com>
2022-05-06 10:55:33 -0400

    Add files via upload

---
+++ Mknoxproj.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Mknox
+{
+    class yourmom
+    {
+        static void Main(string[] args)
+        {
+            var number = 2;
+            var count = 10;
+            var totalPrice = 29.95f;
+            var character = letter 'A';
+            var firstName = "Matthew";
+            var isWorking = f;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+    }
+}
